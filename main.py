dd7093c16f8c36baaaa9e736d5c2b6af516c2e2b
Josh <bacon.josh09@gmail.com>
2016-07-07 18:49:42 -0400

    Added first "command"

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import telepot
 import time
 import emoji
+import json
 
 def handle(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
+    content_type, chat_type, chat_id,  = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
-
+    message = msg['text']
     if content_type == 'text':
-        bot.sendMessage(chat_id, msg['text'])
+        #checks if text is equal to 'hi howard, how are you?', ignoring case
+        if message.lower() == 'hi howard, how are you?'.lower():
+            #replies to message
+            bot.sendMessage(chat_id, "I'm doing just fine, thanks.")
+
 
 
 TOKEN  = '259115532:AAHav6iX1Kgo53ECfUOBQpjMbz_oIX9gFek'
 bot = telepot.Bot(TOKEN)
 bot.message_loop(handle)
 print('Listening...')
-
-bot.sendMessage(209854694, emoji.snowman)
 
 while 1:
     time.sleep(10)

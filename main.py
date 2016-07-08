3a89b0a1e84d4e0601c012f67ab7b711a8344273
2SmoothForYou <jricart2001@gmail.com>
2016-07-07 23:09:41 -0500

    Checking for words in context

--- main.py
+++ main.py
@@ -10,14 +10,19 @@ def handle(msg):
 
     if content_type == 'text':
         message = msg['text']
+        input = message.upper()
+
+        if 'LINNEA' in input:
+            bot.sendMessage(chat_id, '*Linnea' + unicode.crown)
+
         #checks if text is equal to 'hi howard, how are you?', ignoring case
         if message.lower() == 'hi howard, how are you?'.lower():
             #replies to message
             bot.sendMessage(chat_id, "I'm doing just fine, thanks.")
         if message.lower() == 'shut up howard'.lower():
             bot.sendMessage(chat_id, unicode.het)
-        if message.lower() == 'linnea'.lower():
-            bot.sendMessage(chat_id, '*Linnea' + unicode.crown)
+        #if message.lower() == 'linnea'.lower():
+           # bot.sendMessage(chat_id, '*Linnea' + unicode.crown)
         if message.lower() == 'howard, tell me a joke'.lower():
             bot.sendMessage(chat_id, 'Once there was a snail who was tired of being slow. He went out and bought a really fast sports car and had the dealer paint a big S on each side of it. Whenever someone saw him zooming past in his new car, they would say, "Hey, look at that S-car go!"')
 

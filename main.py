604543f095935f596ad9e13cb10f94acc05b54f3
Josh <bacon.josh09@gmail.com>
2016-07-07 21:08:53 -0400

    Made him actually respond

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ import json
 def handle(msg):
     content_type, chat_type, chat_id,  = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
-    message = msg['text']
+
     if content_type == 'text':
+        message = msg['text']
         #checks if text is equal to 'hi howard, how are you?', ignoring case
         if message.lower() == 'hi howard, how are you?'.lower():
             #replies to message
             bot.sendMessage(chat_id, "I'm doing just fine, thanks.")
+        if message.lower() == 'shut up howard'.lower():
+            bot.sendMessage(chat_id, emoji.het)
 
 
 

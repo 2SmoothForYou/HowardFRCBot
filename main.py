77dfc8fe5e12d66a9dfa4698f84bff27237f2279
2SmoothForYou <jricart2001@gmail.com>
2016-07-08 01:54:02 -0500

    Added replying

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 import TOKEN
 
 def handle(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
+    content_type, chat_type, chat_id, msg_date, msg_id = telepot.glance(msg, long=True)
     print(content_type, chat_type, chat_id)
 
     jaimeID = 205253997
@@ -19,13 +19,13 @@ def handle(msg):
         input = message.upper()
 
         if '@HOWARD_G' in input:
-            bot.sendMessage(chat_id, 'Someone called for me?')
+            bot.sendMessage(chat_id, 'Someone called for me?', reply_to_message_id = msg_id)
         if id == jaimeID and 'I' in input and 'LOVE' in input and 'JOSH' in input:
-            bot.sendMessage(chat_id, "But don't tell Linnea" + unicode.crown + " " + unicode.wink)
+            bot.sendMessage(chat_id, "But don't tell Linnea" + unicode.crown + " " + unicode.wink, reply_to_message_id = msg_id)
         if 'GET' in input and 'REKT' in input:
             bot.sendMessage(chat_id, 'DE_STROYED')
         if 'LINNEA' in input and 'LINNEA' + unicode.crown not in input and '@LINNEAW' not in input:
-            bot.sendMessage(chat_id, '*Linnea' + unicode.crown)
+            bot.sendMessage(chat_id, '*Linnea' + unicode.crown, reply_to_message_id = msg_id)
         if 'HOWARD' in input:
             if 'HOW' in input and 'ARE' in input and 'YOU' in input:
                 bot.sendMessage(chat_id, "I'm doing just fine, thanks.")

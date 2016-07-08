b7c297b6c89f972962ea562b7b1d1aec292092db
2SmoothForYou <jricart2001@gmail.com>
2016-07-07 23:41:18 -0500

    Checks everything for context

--- main.py
+++ main.py
@@ -14,17 +14,13 @@ def handle(msg):
 
         if 'LINNEA' in input and 'LINNEA' + unicode.crown not in input:
             bot.sendMessage(chat_id, '*Linnea' + unicode.crown)
-
-        #checks if text is equal to 'hi howard, how are you?', ignoring case
-        if message.lower() == 'hi howard, how are you?'.lower():
-            #replies to message
-            bot.sendMessage(chat_id, "I'm doing just fine, thanks.")
-        if message.lower() == 'shut up howard'.lower():
-            bot.sendMessage(chat_id, unicode.het)
-        #if message.lower() == 'linnea'.lower():
-           # bot.sendMessage(chat_id, '*Linnea' + unicode.crown)
-        if message.lower() == 'howard, tell me a joke'.lower():
-            bot.sendMessage(chat_id, 'Once there was a snail who was tired of being slow. He went out and bought a really fast sports car and had the dealer paint a big S on each side of it. Whenever someone saw him zooming past in his new car, they would say, "Hey, look at that S-car go!"')
+        if 'HOWARD' in input:
+            if 'HOW' in input and 'ARE' in input and 'YOU' in input:
+                bot.sendMessage(chat_id, "I'm doing just fine, thanks.")
+            if 'SHUT' in input and 'UP' in input:
+                bot.sendMessage(chat_id, unicode.het)
+            if 'JOKE' in input and 'TELL' in input:
+                bot.sendMessage(chat_id, 'Once there was a snail who was tired of being slow. He went out and bought a really fast sports car and had the dealer paint a big S on each side of it. Whenever someone saw him zooming past in his new car, they would say, "Hey, look at that S-car go!"')
 
 TOKEN  = TOKEN.token
 bot = telepot.Bot(TOKEN)
